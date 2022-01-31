fe213058bffdac63e4203e1cdfd9f480dffd69a9
Westly Ward <sonicrules1234@gmail.com>
2022-01-31 07:30:20 -0700

    Now writes the dist config file on when it can't fine a config.  Also changed the dist config file to be more intuitive.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use serde::Deserialize;
-
+use std::io::Write;
 #[derive(Clone, Debug, Deserialize)]
 struct SonicbotConfig {
     username: String,
@@ -12,6 +12,13 @@ struct SonicbotConfig {
 }
 
 fn main() {
+    let dist_config = include_str!("../config.yaml.dist");
+    if !std::path::Path::new("config.yaml").exists() {
+        eprintln!("Could not find config file (config.yaml).  Creating blank config...\nPlease fill out config.yaml in the current directory.");
+        let mut f = std::fs::File::create("config.yaml").unwrap();
+        f.write_all(dist_config.as_bytes()).unwrap();
+        return;
+    }
     let sonicbot_config: SonicbotConfig = serde_yaml::from_str(&std::fs::read_to_string("config.yaml").unwrap()).unwrap();
     //println!("{:#?}", sonicbot_config);
     let mut inst = sonicbot_matrix::SonicBot::new(sonicbot_config.host, sonicbot_config.username, sonicbot_config.server_name, true, sonicbot_config.prefix, sonicbot_config.owner);
